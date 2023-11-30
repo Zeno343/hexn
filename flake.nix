d5e28bd7b2d3d12c2406433871efd10ba56b52fb
zeno <zacheryklinglesmith@gmail.com>
2023-11-30 18:11:16 -0500

    Separate smartRelease into ci package

--- flake.nix
+++ flake.nix
@@ -4,11 +4,11 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    crane.url = "github:ipetkov/crane";
+    craneLib.url = "github:ipetkov/crane";
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, crane, rust-overlay }:
+  outputs = { self, nixpkgs, craneLib, rust-overlay }:
   let 
     system = "x86_64-linux";
     pkgs = import nixpkgs {
@@ -16,24 +16,9 @@
       overlays = [ (import rust-overlay) ];
     };
 
-    craneLib = (crane.mkLib pkgs).overrideToolchain pkgs.rust-bin.nightly.latest.default;
-
-    smartRelease = with pkgs; craneLib.buildPackage {
-      src = fetchFromGitHub {
-        owner = "Byron";
-        repo = "cargo-smart-release";
-	rev = "82c39db";
-	sha256 = "sha256-3aS0/2A4O+IDpFXsCup3OlhlvWuiMf0M1USJqnjAR7w=";
-      };
-
-      buildInputs = [ openssl ];
-      nativeBuildInputs = [ cmake pkg-config ];
-    };
-
-    metaData = with pkgs; rec {
-      pname = "hexen";
-      version = "v0.0.1";
-      src = craneLib.path ./.;
+    crane = (craneLib.mkLib pkgs).overrideToolchain pkgs.rust-bin.nightly.latest.default;
+    hexenSrc = with pkgs; rec {
+      src = crane.path ./.;
       cargoTestExtraArgs = "--all-features";
 
       LIBCLANG_PATH = "${llvmPackages_15.libclang.lib}/lib";
@@ -47,21 +32,39 @@
       nativeBuildInputs = [
         pkg-config
         llvmPackages_15.libcxxClang
-        smartRelease
       ];
     };
 
-    hexenDeps = craneLib.buildDepsOnly metaData;
-    hexen = craneLib.buildPackage (metaData // {
+    ci = let
+      smartRelease = with pkgs; crane.buildPackage {
+        src = fetchFromGitHub {
+          owner = "Byron";
+          repo = "cargo-smart-release";
+	  rev = "82c39db";
+	  sha256 = "sha256-3aS0/2A4O+IDpFXsCup3OlhlvWuiMf0M1USJqnjAR7w=";
+        };
+
+        buildInputs = [ openssl ];
+        nativeBuildInputs = [ cmake pkg-config ];
+      };
+    in crane.buildDepsOnly (hexenSrc // {
+      cargoArtifacts = [ hexenDeps smartRelease ];
+    });
+
+
+    hexenDeps = crane.buildDepsOnly hexenSrc;
+    hexen = crane.buildPackage (hexenSrc // {
       cargoArtifacts = hexenDeps;
     });
   in {
-    packages.${system}.default = hexen;
-
-    devShells.${system}.default = craneLib.devShell {
-      LIBCLANG_PATH = "${pkgs.llvmPackages_15.libclang.lib}/lib";
-      name = "hexen";
-      inputsFrom = [ hexen ];
+    packages.${system} = {
+      default = hexen;
+      inherit ci;
     };
+
+    devShells.${system}.default = crane.devShell (hexenSrc // {
+      name = "hexen-ci";
+      cargoArtifacts = ci;
+    });
   };
 }
