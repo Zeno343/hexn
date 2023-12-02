6603add995c201bca72c4cbdd0881188796528d9
zeno <zacheryklinglesmith@gmail.com>
2023-12-02 01:12:55 -0500

    Rename hexen -> hexn

--- build.rs
+++ build.rs
@@ -1,8 +1,8 @@
 fn main() {
     if cfg!(debug_assertions) {
-        println!("cargo:rustc-cfg=feature=\"std\"");
+        println!(r#"cargo:rustc-cfg=feature="std""#);
     } else if cfg!(not(feature = "std")) {
-        println!("cargo:rustc-link-arg-bin=hexen=-nostartfiles");
+        println!("cargo:rustc-link-arg-bin=hexn=-nostartfiles");
         println!("cargo:rustc-link-lib=c");
     }
 }
